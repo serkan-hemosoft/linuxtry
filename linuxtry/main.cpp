7603b64efef846814f18168f18f83d32a8dd9b00
serkan <serkan@ASUSERK>
2016-06-22 22:32:45 +0300

    Add project files.

---
+++ linuxtry/main.cpp
@@ -0,0 +1,8 @@
+#include <cstdio>
+
+int main()
+{
+	for(int i=0;i<5;i++)
+      printf("hello %d\n", i);
+    return 0;
+}
---
+++ wintry/wintry.cpp
@@ -0,0 +1,13 @@
+// wintry.cpp : Defines the entry point for the console application.
+//
+
+//#include "stdafx.h"
+#include <stdio.h>
+
+int main()
+{
+	for (int i = 0;i<5;i++)
+		printf("hello %d\n", i);
+	return 0;
+}
+
